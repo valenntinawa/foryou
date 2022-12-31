902d5abe6d5e4d6bf741c70183d339892a882020
valenntinawa <121645635+valenntinawa@users.noreply.github.com>
2022-12-31 18:52:58 +0700

    Add files via upload

---
+++ stylePopUp.css
@@ -0,0 +1,30 @@
+.swal2-popup {
+  border-radius: 20px !important;
+  -webkit-border-radius: 20px !important;
+  -moz-border-radius: 20px !important;
+  -ms-border-radius: 20px !important;
+  -o-border-radius: 20px !important;
+}
+
+.swal2-title {
+  font-size: 1.6em !important;
+}
+
+.swal2-html-container {
+  height: 1.6em !important;
+}
+
+.swal2-styled {
+  color: white !important;
+  background: #894eff !important;
+  padding: 10px 20px;
+  border-radius: 10px !important;
+  -webkit-border-radius: 10px !important;
+  -moz-border-radius: 10px !important;
+  -ms-border-radius: 10px !important;
+  -o-border-radius: 10px !important;
+}
+
+.swal2-image {
+  height: 150px !important;
+}
